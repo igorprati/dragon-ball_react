99e7d59eba35411962c5a0d2ca055fb44c05cad4
Igor Gregorio Prati <igorprati@hotmail.com>
2021-08-25 16:14:54 -0300

    criação página about

---
+++ src/Pages/About/styled.js
@@ -0,0 +1,31 @@
+import styled from "styled-components";
+
+export const Center = styled.div`
+	text-align: center;
+	max-width: 960px;
+	margin: 0 auto;
+	/* display: flex;
+	flex-wrap: wrap; */
+`;
+
+export const Text = styled.p`
+	font-size: 1.3rem;
+	margin: 1rem;
+`;
+
+export const Image = styled.img`
+	max-width: 400px;
+	max-height: 400px;
+	border-radius: 1rem;
+	object-fit: cover;
+`;
+
+export const IconBox = styled.div`
+	display: flex;
+	justify-content: center;
+`;
+
+export const Icons = styled.img`
+	width: 40px;
+	margin: 1rem 0.5rem;
+`;
--- src/Pages/ReadAll/ReadAll.jsx
+++ src/Pages/ReadAll/ReadAll.jsx
@@ -17,8 +17,6 @@ export default function ReadAll() {
         fetchApi()
     }, [])
 
-    console.log(data)
-
     return (
         <>
         {loading === true ? (
